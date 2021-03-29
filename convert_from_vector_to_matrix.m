ac86819fd4c081f9badc9250a076cf68c4f02522
Jonas Weigand <46886536+JonasWeigand@users.noreply.github.com>
2021-03-29 13:16:54 +0200

    Delete convert_from_vector_to_matrix.m

--- convert_from_vector_to_matrix.m
+++
@@ -1,9 +0,0 @@
-% generate a toeplitz matrix from vector
-    function H = convert_from_vector_to_matrix( h_vec , time)
-        
-        first_row_h = [h_vec(1), zeros(1,time.nt-1)];
-        first_colum_h = h_vec;
-        
-        H = toeplitz(first_colum_h, first_row_h);
-        
-    end
